bc1ccf98051f8efa1737ca6c4ec53cbdd9b71511
Cycofactory <Cycofactory@e3c562b9-017a-70de-d5fb-b8ac69c58711>
2011-12-01 19:57:04 +0000

    There are things that need to be merged in located in the .txt files in src

--- trunk/MidiSynth/src/MidiTest.java
+++ trunk/MidiSynth/src/MidiTest.java
@@ -1,24 +1,29 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Random;
 
 import javax.sound.midi.Instrument;
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class MidiTest {
 
 	MidiThread thread;
 	boolean on = false;
+	int i, count;
 	String[] toggleText = {"Play", "Stop"};
-	long seed;
+	static long seed;
 	boolean seedSet = false;
 
 	public static void main(String[] args) {        
@@ -35,12 +40,45 @@ public class MidiTest {
 			synth.loadInstrument(instr[90]);
 
 			JFrame frame = new JFrame("Midi Sample");                
-			JPanel pane = new JPanel();                         
+			JPanel pane = new JPanel();
+
+			JPanel ratePanel = new JPanel();
+			ButtonGroup ratingGroup = new ButtonGroup();
+			JRadioButton[] rating = new JRadioButton[10];
+
+			for(i = 0; i < rating.length; i++){
+				rating[i] = new JRadioButton("" + (i+1));
+				rating[i].setMnemonic(49 + i);
+				if(i == 9)
+					rating[i].setMnemonic(48);
+				rating[i].addActionListener(new ActionListener() {
+
+					int id = i+1;
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						try {
+							PrintWriter out = new PrintWriter(new FileOutputStream("rates.txt", true));
+							out.println(seed + " " + id);
+							out.close();
+
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						}
+
+					}
+
+				});
+				ratingGroup.add(rating[i]);
+				ratePanel.add(rating[i]);
+			}
+
+
 			JButton button1 = new JButton(toggleText[0]);
 			JButton button2 = new JButton("Set Seed");
 			frame.getContentPane().add(pane);
 			pane.add(button1);
 			pane.add(button2);
+			pane.add(ratePanel);
 			frame.pack();                                       
 			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 			frame.show();
@@ -48,7 +86,9 @@ public class MidiTest {
 			button1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					if (!on) {
-						thread = new MidiThread(mc, seedSet ? seed : ((long)((Math.random()-.5)*Long.MAX_VALUE*2)));
+						if(!seedSet)
+							seed = ((long)((Math.random()-.5)*Long.MAX_VALUE*2));
+						thread = new MidiThread(mc, seed);
 						thread.start();
 						seedSet = false;
 					} else {
@@ -57,7 +97,7 @@ public class MidiTest {
 					on = !on;
 					((JButton)(e.getSource())).setText(toggleText[on ? 1 : 0]);
 				}});    // END OF THE ACTION LISTENER
-			
+
 			button2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					seed = Long.parseLong(JOptionPane.showInputDialog(((JButton)(e.getSource())).getParent(), "Input seed:"));
@@ -75,15 +115,15 @@ public class MidiTest {
 class MidiThread extends Thread {
 	MidiChannel[] mc;
 	Random rand;
-	
+
 	public MidiThread(MidiChannel[] mc, long seed) {
 		this.mc = mc;
 		rand = new Random(seed);
 		System.out.println("Seed: "+seed);
 	}
-	
+
 	public void run() {
-		int length = (int)(rand.nextDouble()*6)+4;
+		int length = (int)(rand.nextDouble()*5)+6;
 
 		int[] note = new int[length];
 		int[] time = new int[length];
