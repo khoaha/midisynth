8e36349e29db9c7b9fa9c4ec344dab5bab68200f
anrodger@terpmail.umd.edu <anrodger@terpmail.umd.edu@e3c562b9-017a-70de-d5fb-b8ac69c58711>
2011-12-06 00:50:57 +0000

    Made MidiTest be able to read from files in seed or array format. Also some code restructuring.

--- trunk/MidiSynth/src/MidiTest.java
+++ trunk/MidiSynth/src/MidiTest.java
@@ -3,8 +3,6 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Random;
 
 import javax.sound.midi.Instrument;
 import javax.sound.midi.MidiChannel;
@@ -25,128 +23,136 @@ public class MidiTest {
 	String[] toggleText = {"Play", "Stop"};
 	static long seed;
 	boolean seedSet = false;
+	
+	protected String filename;
+	protected Format fileformat;
+	
+	MidiChannel[] mc;
 
 	public static void main(String[] args) {        
-		new MidiTest();
+		new MidiTest(Format.NOTFILE, "crowd1_sort.txt");
 	}
 
-	public MidiTest() {		
+	public MidiTest(Format fileformat, String filename) {		
 		try {
+			this.fileformat = fileformat;
+			this.filename = filename;
+			
 			Synthesizer synth = MidiSystem.getSynthesizer();
 			synth.open();
-
-			final MidiChannel[] mc = synth.getChannels();
+			
+			mc = synth.getChannels();
 			Instrument[] instr = synth.getDefaultSoundbank().getInstruments();		
 			synth.loadInstrument(instr[90]);
 
 			JFrame frame = new JFrame("Midi Sample");                
+			
 			JPanel pane = new JPanel();
+			
+			generateGUI(frame, pane);
 
-			JPanel ratePanel = new JPanel();
-			ButtonGroup ratingGroup = new ButtonGroup();
-			JRadioButton[] rating = new JRadioButton[10];
-
-			for(i = 0; i < rating.length; i++){
-				rating[i] = new JRadioButton("" + (i+1));
-				rating[i].setMnemonic(49 + i);
-				if(i == 9)
-					rating[i].setMnemonic(48);
-				rating[i].addActionListener(new ActionListener() {
-
-					int id = i+1;
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						try {
-							PrintWriter out = new PrintWriter(new FileOutputStream("rates.txt", true));
-							out.println(seed + " " + id);
-							out.close();
-
-						} catch (FileNotFoundException e) {
-							e.printStackTrace();
-						}
-
-					}
-
-				});
-				ratingGroup.add(rating[i]);
-				ratePanel.add(rating[i]);
-			}
-
-
-			JButton button1 = new JButton(toggleText[0]);
-			JButton button2 = new JButton("Set Seed");
-			frame.getContentPane().add(pane);
-			pane.add(button1);
-			pane.add(button2);
-			pane.add(ratePanel);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.pack();                                       
-			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-			frame.show();
-
-			button1.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (!on) {
-						if(!seedSet)
-							seed = ((long)((Math.random()-.5)*Long.MAX_VALUE*2));
-						thread = new MidiThread(mc, seed);
-						thread.start();
-						seedSet = false;
-					} else {
-						thread.stop();
-					}
-					on = !on;
-					((JButton)(e.getSource())).setText(toggleText[on ? 1 : 0]);
-				}});    // END OF THE ACTION LISTENER
-
-			button2.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					seed = Long.parseLong(JOptionPane.showInputDialog(((JButton)(e.getSource())).getParent(), "Input seed:"));
-					seedSet = true;
-				}});    // END OF THE ACTION LISTENER
-
+			frame.setVisible(true);
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
 	}       // END OF THE MAIN METHOD 
-}
 
-class MidiThread extends Thread {
-	MidiChannel[] mc;
-	Random rand;
+	/**
+	 * @param frame
+	 * @param pane
+	 */
+	protected void generateGUI(JFrame frame, JPanel pane) {
+		JPanel ratePanel = createRatePanel();
 
-	public MidiThread(MidiChannel[] mc, long seed) {
-		this.mc = mc;
-		rand = new Random(seed);
-		System.out.println("Seed: "+seed);
+		JButton button1 = new JButton(toggleText[0]);
+		JButton button2 = new JButton("Set Seed");
+		frame.getContentPane().add(pane);
+		pane.add(button1);
+		pane.add(button2);
+		pane.add(ratePanel);
+		
+		addPlayButtonListener(button1);
+
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				seed = Long.parseLong(JOptionPane.showInputDialog(((JButton)(e.getSource())).getParent(), "Input seed:"));
+				seedSet = true;
+			}});    // END OF THE ACTION LISTENER
 	}
 
-	public void run() {
-		int length = (int)(rand.nextDouble()*5)+6;
-
-		int[] note = new int[length];
-		int[] time = new int[length];
-		int[] wait = new int[length];
-
-		for (int i=0; i<length; i++) {
-			note[i] = (int)(rand.nextDouble()*128);
-			time[i] = (int)(rand.nextDouble()*500)+100;
-			wait[i] = (int)(rand.nextDouble()*500)+100;
-		}
-
-		System.out.println("Notes: "+Arrays.toString(note));
-		System.out.println("Times: "+Arrays.toString(time));
-		System.out.println("Waits: "+Arrays.toString(wait));
-
-		while (true) {
-			for (int i=0; i<length; i++) {
-				mc[5].noteOn(note[i], time[i]);
-				try {
-					Thread.sleep(wait[i]);
-				} catch (InterruptedException e1) {
+	/**
+	 * @param button1
+	 */
+	protected void addPlayButtonListener(JButton button1) {
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!on) {
+					createMidiThread(fileformat, filename);
+				} else {
+					thread.shutdown();
 				}
-			}
+				on = !on;
+				((JButton)(e.getSource())).setText(toggleText[on ? 1 : 0]);
+			}});    // END OF THE ACTION LISTENER
+	}
+
+	/**
+	 * @return
+	 */
+	protected JPanel createRatePanel() {
+		JPanel ratePanel = new JPanel();
+		ButtonGroup ratingGroup = new ButtonGroup();
+		JRadioButton[] rating = new JRadioButton[10];
+
+		for(i = 0; i < rating.length; i++){
+			rating[i] = new JRadioButton("" + (i+1));
+			rating[i].setMnemonic(49 + i);
+			if(i == 9)
+				rating[i].setMnemonic(48);
+			rating[i].addActionListener(new ActionListener() {
+
+				int id = i+1;
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						PrintWriter out = new PrintWriter(new FileOutputStream("rates.txt", true));
+						out.println(seed + " " + id);
+						out.close();
+
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+
+				}
+
+			});
+			ratingGroup.add(rating[i]);
+			ratePanel.add(rating[i]);
 		}
+		return ratePanel;
+	}
+
+	/**
+	 * 
+	 */
+	protected void createMidiThread(Format fileformat, String filename) {
+		/* For creating random loops, uncomment and add another field to Format */
+		switch(fileformat) {
+		case NOTFILE:
+			if(!seedSet)
+				seed = ((long)((Math.random()-.5)*Long.MAX_VALUE*2));
+			thread = new MidiThread(mc, seed);
+			seedSet = false;
+			break;
+		case ARRAY:
+		case SEED:
+			thread = new MidiThread(mc, filename, fileformat);
+		}
+		
+		thread.start();
 	}
 }
---
+++ trunk/MidiSynth/src/MidiThread.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.sound.midi.MidiChannel;
+
+class MidiThread extends Thread {
+	MidiChannel[] mc;
+	Random rand;
+	protected Format fileformat;
+	protected String filename;
+	
+	protected PlayerThread player;
+
+	public MidiThread(MidiChannel[] mc, long seed) {
+		this.mc = mc;
+		rand = new Random(seed);
+		System.out.println("Seed: "+seed);
+		fileformat = Format.NOTFILE;
+	}
+	
+	public MidiThread(MidiChannel[] mc, String filename, Format fileformat) {
+		this.mc = mc;
+		this.fileformat = fileformat;
+		this.filename = filename;
+	}
+
+	public void run() {
+		int[][] loop = new int[3][]; 
+		
+		switch(fileformat) {
+		case NOTFILE:
+			int length;
+			
+			length = (int)(rand.nextDouble()*5)+6;
+
+			loop[0] = new int[length]; /* note */
+			loop[1] = new int[length]; /* time */
+			loop[2] = new int[length]; /* wait */
+
+			createLoop(length, loop[0], loop[1], loop[2]);
+			break;
+		case ARRAY:
+		case SEED:
+			loop = SequenceInferer.createLoopFromFile(filename, fileformat);
+		}
+		
+		player = new PlayerThread(loop);
+		player.start();
+		
+		try {
+			player.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param length
+	 * @param note
+	 * @param time
+	 * @param wait
+	 */
+	protected void createLoop(int length, int[] note, int[] time, int[] wait) {
+		for (int i=0; i<length; i++) {
+			note[i] = (int)(rand.nextDouble()*128);
+			time[i] = (int)(rand.nextDouble()*500)+100;
+			wait[i] = (int)(rand.nextDouble()*500)+100;
+		}
+
+		System.out.println("Notes: "+Arrays.toString(note));
+		System.out.println("Times: "+Arrays.toString(time));
+		System.out.println("Waits: "+Arrays.toString(wait));
+	}
+	
+	public void shutdown() {
+		player.shutdown();
+	}
+}
